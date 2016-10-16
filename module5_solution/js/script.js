d579fa6ebf02ec8adc518c9eb1bff688876dce67
Antonia <apatter7@gmail.com>
2016-10-15 21:21:58 -0400

    update script.js

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
 
-        
+         var homeHtmlToInsertIntoMainPage =insertPropery (homeHtml, "randomCategoryShortName","" + chosenCategoryShortName + "");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
@@ -124,8 +124,7 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
      
-      insertHtml ("#main-content",homeHtmlToInsertIntoMainPage);
-
+  
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
